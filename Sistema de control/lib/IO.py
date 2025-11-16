380baba4933aff4b36b73304b2a995483d7db502
UrielOne10 <uriel.diez@hotmail.es>
2025-11-16 14:13:48 -0600

    update IO

--- Sistema de control/lib/IO.py
+++ Sistema de control/lib/IO.py
@@ -1,17 +1,16 @@
 from machine import Pin,ADC
 import neopixel
+import time
 
-
-config = {"Btn_arriba":{"pin":4,"modo":Pin.IN,"pull":Pin.PULL_UP},
-          "Btn_abajo":{"pin":5,"modo":Pin.IN,"pull":Pin.PULL_UP},
-          "Btn_switch":{"pin":6,"modo":Pin.IN,"pull":Pin.PULL_UP},
-          "Btn_clk":{"pin":7,"modo":Pin.IN,"pull":Pin.PULL_UP}
+config = {"Btn_on_off":{"pin":4,"modo":Pin.IN,"pull":Pin.PULL_UP},
+          "Btn_enter":{"pin":5,"modo":Pin.IN,"pull":Pin.PULL_UP},
+          "Btn_clk":{"pin":7,"modo":Pin.IN,"pull":Pin.PULL_UP},
           "Btn_D":{"pin":0,"modo":Pin.IN,"pull":Pin.PULL_UP}
           }
-configIRQ = {"Btn_arriba":{"trigger":Pin.IRQ_FALLING},
-             "Btn_focus_up":{"trigger":Pin.IRQ_FALLING},
-             "Btn_switch":{"trigger":Pin.IRQ_FALLING},
-             "Btn_D":{"trigger":Pin.IRQ_FALLING}}
+configIRQ = {"Btn_on_off":{"trigger":Pin.IRQ_FALLING},
+             "Btn_enter":{"trigger":Pin.IRQ_FALLING},
+             "Btn_D":{"trigger":Pin.IRQ_FALLING}
+             }
 pines = {}
 
 np=None
@@ -27,5 +26,46 @@ def init_pines():
 def init_irq(clave, _handler):
     valor_trigger = configIRQ[clave]["trigger"]
     pines[clave].irq(handler=_handler,trigger = valor_trigger)
-    
+
+def get_value(pin):
+    return pines[pin].value()
+
+def set_color(r, g, b):
+    global np
+    np[0] = (r, g, b)
+    np.write()
+
+def configRGB():
+    global np
+    NEO_PIN = 8          
+    NUM_LEDS = 1
+    np = neopixel.NeoPixel(Pin(NEO_PIN), NUM_LEDS)   
+
+last_ms = {}
+def antirrebote(nombre, intervalo_ms = 200):
+    """
+    Devuelve True si ya pasó suficiente tiempo desde el último evento 'nombre'.
+    Devuelve False si se considera rebote.
+    """
+    global last_ms
+    rebote = False
+    now = time.ticks_ms()
+    last = last_ms.get(nombre, 0)
+
+    if time.ticks_diff(now, last) < intervalo_ms:
+        rebote = False  # rebote
+    else:
+        #print("entro")
+        last_ms[nombre] = now
+        rebote = True
+    return rebote
+
+def encoder_procesar():
+    cmd = None
+    valor_clk = get_value("Btn_clk") 
+    if valor_clk == 1:
+        cmd = {"focus":"arriba"}
+    else:
+        cmd = {"focus":"abajo"}
+    return cmd
 
