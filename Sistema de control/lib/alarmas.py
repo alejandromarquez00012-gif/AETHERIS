250e6663e62573ef38c358406c004667934a1de4
UrielOne10 <uriel.diez@hotmail.es>
2025-11-16 14:14:58 -0600

    update alarmas

--- Sistema de control/lib/alarmas.py
+++ Sistema de control/lib/alarmas.py
@@ -1,7 +1,16 @@
 from machine import Pin
-
-def alarmas_init():
-    None
+rangos = {"aceptable":{"min":95,"max":100},
+          "regular":{"min":90,"max":95},
+          "bajo":{"min":87,"max":90},
+          "En riesgo":{"min":15,"max":87}}
+#{"rx":"alarmas","aceptable":{"min":95,"max":100},"regular":{"min":90,"max":95},"bajo":{"min":87,"max":90},"En riesgo":{"min":15,"max":87}}
+def alarmas_set(cmd):
+    global rangos
+    for clave, limites in rangos.items():
+        rangos[clave] = cmd[clave]
+def alarmas_print():
+    for clave,limites in rangos.items():
+        print(f"la clave es: {clave} y sus limites: {limites}")
 def alarmas_on():
     None
 def alarmas_off():
